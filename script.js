052175f27c74a6adabe9c2b50c76ccab9cf70799
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-11 17:13:16 -0700

    before for loop

--- script.js
+++ script.js
@@ -4,13 +4,31 @@
 window.onload = function() {
 
 	//alert("javascript");
+	// true is X's turn
+	var turn = true;
 
 	var boxes = document.getElementsByClassName("box");
 
-	boxes[0].onclick = function() {
-		this.innerHTML = "O";
-	};
+	// boxes[0].onclick = function() {
+	// 	this.innerHTML = "O";
+	// };
+	// boxes[1].onclick = function() {
+	// 	this.innerHTML = "X";
+	// };
 
+	boxes[0].onclick = function() {
+		// put an X
+		if (turn == true) {
+			this.class = "x";
+			this.innerHTML = "X";
+			turn = false;
+		} else {
+			//put an O
+			this.class = "o";			
+			this.innerHTML = "O";
+			turn = true;
+		}
+	};
 
 
 
