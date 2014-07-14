f4edfff0965dae230f40614ad88234feb1f9459c
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-13 22:03:41 -0700

    small tweaks around next player

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 // javascript for tic tac toe
+// by Lisa Vogt, https://github.com/lisavogtsf/tictactoe_js
 
 window.onload = function() {
 
@@ -8,22 +9,28 @@ window.onload = function() {
 	var turn = true;
 	var reset = document.getElementById("reset");
 	var displayNext = document.getElementById("displayNext");
+	displayNext.innerHTML = "X";
 	// put box elements into array
 	var boxes = document.getElementsByClassName("box");
 
-// reset affects class
+	// reset affects class
 	reset.onclick = function(event) {
 		//	alert("reset");
 		for(var j = 0; j < boxes.length; j++){
 			boxes[j].className = "box";
 			boxes[j].innerHTML = "";
 		}
+		displayNext.innerHTML = "X";
 	}
 
-
+	// actions for when boxes are clicked on
+	// for loop makes sure all boxes are listening
 	for(var i = 0; i < boxes.length; i++){
 		boxes[i].onclick = function() {
-
+			// react to clicks by marking X or O using class
+			// once x or o class has been added, the item 
+			// no longer has className box, it is box x
+			// so a mark only goes up if the box is empty
 			if (this.className === "box") {
 				// put an X
 				if (turn === true) {
